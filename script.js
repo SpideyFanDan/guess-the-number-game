cb83aac4fd18dacc611a1ce22933619884dfcf82
SpideyFanDan <dsmith.teach@gmail.com>
2021-03-11 21:39:00 -0500

    added guess history functionality

--- script.js
+++ script.js
@@ -4,21 +4,22 @@
  * DONE: Generate a random number 1 to 100 and save it to variable correctNumber
  * DONE: Console whether the guess is too high, too low, or is correct inside playGame function
  * DONE: Create a function called displayResult to move the logic for if the guess is too high, too low, or correct
- * TODO: Complete the showYouWon, showNumberAbove, showNumberBelow
- * TODO: Use the showYouWon... functions within displayResult to display the correct dialog
- * TODO: Save the guess history in a variable called guess
- * TODO: Display the guess history using displayHistory() function
+ * DONE: Complete the showYouWon, showNumberAbove, showNumberBelow
+ * DONE: Use the showYouWon... functions within displayResult to display the correct dialog
+ * DONE: Save the guess history in a variable called guess
+ * DONE: Display the guess history using displayHistory() function
  * TODO: Use the initGame() function to restart the game
  */
 
 // Variable to store the list of guesses
-
+let guesses = [];
 // Variable for store the correct random number
 let correctNumber = getRandomNumber();
 
 window.onload = function () {
 	document.getElementById('number-submit').addEventListener('click', playGame);
 	document.getElementById('restart-game').addEventListener('click', initGame);
+	console.log(correctNumber);
 };
 
 /**
@@ -28,6 +29,8 @@ function playGame() {
 	// *CODE GOES BELOW HERE *
 	let numberGuess = document.getElementById('number-guess').value;
 	displayResult(numberGuess);
+	saveGuessHistory(numberGuess);
+	displayHistory();
 }
 
 /**
@@ -77,6 +80,7 @@ function getRandomNumber() {
  */
 function saveGuessHistory(guess) {
 	// *CODE GOES BELOW HERE *
+	guesses.push(guess);
 }
 
 /**
@@ -88,9 +92,17 @@ function saveGuessHistory(guess) {
  * HINT: use while loop and string concatentation to create a list of guesses
  */
 function displayHistory() {
-	let index; // TODO
+	let index = guesses.length - 1; // TODO
 	let list = "<ul class='list-group'>";
 	// *CODE GOES BELOW HERE *
+	while (index >= 0) {
+		list +=
+			"<li class='list-group-item'>" +
+			'You guessed ' +
+			guesses[index] +
+			'</li>';
+		index -= 1;
+	}
 	list += '</ul>';
 	document.getElementById('history').innerHTML = list;
 }
